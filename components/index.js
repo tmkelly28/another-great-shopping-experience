97bf02accb6bebbc0d59f94300d6b98de0eb5ae8
Thomas Kelly <tmkelly28@gmail.com>
2018-04-05 16:48:40 -0400

    saving

--- components/index.js
+++ components/index.js
@@ -3,17 +3,46 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import store from './store'
+import {Provider, connect} from 'react-redux'
+import store, {fetchPupsFromServer} from './store'
+
+const Puppies = (props) => {
+  const puppies = props.puppies
+  const fetchPups = props.fetchPups
+
+  return (
+    <ul>
+      {
+        puppies
+          .map(pup => <li key={pup.id}>{pup.name}</li>)
+      }
+      <button onClick={fetchPups}>Fetch those pups!</button>
+    </ul>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    puppies: state.puppies
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchPups: () => {
+      const thunk = fetchPupsFromServer()
+      dispatch(thunk)
+    }
+  }
+}
+
+const ConnectedPuppies = connect(mapStateToProps, mapDispatchToProps)(Puppies)
 
 ReactDOM.render(
   <Provider store={store}>
     <div id='demo' className='fill-xy bg-purple white column center-xy'>
       <div>
-        <div>Actions&</div>
-        <div>Reducers&</div>
-        <div>Middleware&</div>
-        <div>Redux.</div>
+        <ConnectedPuppies />
       </div>
     </div>
   </Provider>,
--- components/store.js
+++ components/store.js
@@ -3,10 +3,32 @@ import logger from 'redux-logger'
 import thunks from 'redux-thunk'
 import axios from 'axios'
 
-const initialState = {}
+const GOT_PUPS = 'GOT_PUPS'
+
+export const gotPups = (puppies) => ({
+  type: GOT_PUPS,
+  puppies
+})
+
+export const fetchPupsFromServer = () => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(gotPups([{id: 1, name: 'Cody'}]))
+    }, 1000)
+  }
+}
+
+const initialState = {
+  puppies: []
+}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case GOT_PUPS:
+      return {
+        ...state,
+        puppies: action.puppies
+      }
     default:
       return state
   }
